15edbb7f4befcdd0131544b1480920e7eb2cbc2d
YevhenSerdiuk2022 <99655492+YevhenSerdiuk2022@users.noreply.github.com>
2022-10-11 14:07:44 +0300

    add

---
+++ js/01-gallery.js
@@ -0,0 +1,54 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createPictureCardMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup)
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+
+function createPictureCardMarkup (gallerys) {
+    return galleryItems.map(({description, original, preview
+
+}) => {
+        return `
+        <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+      </a>
+    </div>
+    `;
+    }).join('');
+    
+}
+
+function onGalleryContainerClick (evt) {
+    evt.preventDefault();
+    const isCardEl = evt.target.classList.contains('gallery__image');
+    if (!isCardEl) {
+        return;
+    }
+
+    const selectedTmage = evt.target.dataset.source;
+
+    modalWindowlGallery(selectedTmage);
+    
+} 
+
+function modalWindowlGallery(select) {
+     const instance = basicLightbox.create(`
+    <img src="${select}" width="800" height="600">
+`)
+    instance.show();
+
+    galleryContainer.addEventListener('keydown', evt => {
+		if (evt.key === 'Escape') {
+			instance.close()
+		}
+	})
+}
+
---
+++ js/02-lightbox.js
@@ -0,0 +1,29 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createPictureCardMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup)
+
+
+function createPictureCardMarkup (gallerys) {
+    return galleryItems.map(({description, original, preview
+
+}) => {
+        return `
+       
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" title="${description}" alt="${description}">
+      </a>
+ 
+    `;
+    }).join('');
+  
+}
+
+new SimpleLightbox('.gallery a', {
+	captionDelay: 250
+})
+
